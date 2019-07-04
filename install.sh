93d25a27636c73138e71c01d65b37a8939ee4c55
Will Jacobs <willjjacobs@gmail.com>
2019-07-03 22:09:15 -0500

    Mild implementation of an install script.

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+#TODO: take arguments on which packages to apply
+cmd="stow -vt ${HOME} bash git"
+output="$(${cmd} 2>&1)"
+# echo "${output}"
+if [ "$output" ]; then
+	echo "$?"
+	conflicts="$(echo "${output}" | grep '^  \*' | awk 'NF>1{print$NF}')"
+	echo -e "Conflicts detected in: ${conflicts}\nPlease fix before trying again."
+	#TODO: display diff for each file in conflicts
+else
+	#TODO: Ask for confirmation before applying
+	${cmd}
+	# Reload the shell (i.e. invoke as a login shell)
+	exec "$SHELL -l"
+fi;
+
+
